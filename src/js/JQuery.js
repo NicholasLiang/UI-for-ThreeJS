470a37e7da97a0d8bf7e23e5cccf520adfb1dc31
Nicholas Liang <nla45@sfu.ca>
2018-02-16 12:52:57 -0800

    add hover over hamburger effect

--- src/js/JQuery.js
+++ src/js/JQuery.js
@@ -5,14 +5,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("navbar").style.height = "0%";
 }
-
-document.getElementById("hamburger").addEventListener("mouseover", mouseOver);
-document.getElementById("hamburger").addEventListener("mouseout", mouseOut);
-
-function mouseOver() {
-    document.getElementById("hamburger").src = "images/hamburger_hover.svg";
-}
-
-function mouseOut() {
-    document.getElementById("hamburger").src = "images/hamburger.svg";
-}
